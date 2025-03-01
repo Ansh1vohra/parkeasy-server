9eafa9dcf680e68052a279f23f3e86f2cdda1827
Ansh Vohra <143844078+Ansh1vohra@users.noreply.github.com>
2025-03-02 01:25:33 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // server/routes/users.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.post('/', async (req, res) => {
   try {
